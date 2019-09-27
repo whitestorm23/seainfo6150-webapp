0b6041d46ab4c427155be97ffd421d6d2fdba01d
Mitras <ma.zef@husky.neu.edu>
2019-09-27 14:15:52 -0700

    my revised assignment

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -6,15 +6,14 @@ const Article = () => {
       <html >
           <head>
               <meta charset="utf-8" />
-              <title>assignment3</title>
-
+              <title>assignment3</title>           
 
           </head>
 
           <body>
               <h1>The Statue of Liberty's torch heads to new museum </h1>
               <p>
-                  by Kate Farley(<a href="kate.farley@nytimes.com">kate.farley@nytimes.com</a>)
+                  <i>by Kate Farley</i>(<a href="kate.farley@nytimes.com">kate.farley@nytimes.com</a>)
               </p>
 
               <time datetime="2018-11-22">November 22, 2018</time>
@@ -22,17 +21,11 @@ const Article = () => {
 
 
               <p>Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was<br /> replaced by a replica in 1984. It's been stored in the statue's pedestal ever since.</p>
-              <p>
-                  But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open<br /> on Liberty Island in May 2019.
-        The torch was transported there by truck last week, along with a replica model of Lady Liberty's<br /> face.
-              </p>
-              <p>
-                  "Although it is not one of the most difficult things we have ever moved," says Douglas Phelps, who oversaw the relocation of <br />the torch, "it is certainly the most important."
-               </p>
+              <p> But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open<br /> on Liberty Island in May 2019.
+                  The torch was transported there by truck last week, along with a replica model of Lady Liberty's<br /> face.</p>
+                    <p>"Although it is not one of the most difficult things we have ever moved," says Douglas Phelps, who oversaw the relocation of <br />the torch, "it is certainly the most important."</p>
               <h1>The history of the Statue of Liberty</h1>
-              <p>
-                  Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation<br / >of _Outstanding Universal Value_.
-              </p>
+                    <p> Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation<br />of _Outstanding Universal Value_.  </p>      
               <p>Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel <br />-- yes, the same guy who built Paris's Eiffel Tower.</p>
              
                 <p>  Lady Liberty was a gift from France to the United States to celebrate the centennial of their nation's independence -- although<br />, somewhat awkwardly
@@ -42,9 +35,8 @@ const Article = () => {
                   "The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," says John <br />Piltzecker, superintendent of the Statue of Liberty National Monument.
                 That symbolic spirit inspired the decision to inscribe <br />these famous lines on the statue's pedestal:</p>
     
-              <p>
-                  &nbsp;&nbsp;&nbsp;&nbsp; Give me your tired, your poor <br />
-                  &nbsp;&nbsp;&nbsp;&nbsp; Your huddled masses yearning to breathe free. </p>
+              <p>&nbsp;&nbsp;&nbsp;&nbsp; Give me your tired, your poor <br />
+                 &nbsp;&nbsp;&nbsp;&nbsp; Your huddled masses yearning to breathe free. </p>
 
    
               <p>In 2017, nearly 4.5 million people visited Liberty Island.</p>
